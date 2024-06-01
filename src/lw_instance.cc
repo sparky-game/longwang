6fe98550b86e044296cac6a58bb5c761bca9a9dc
iWas-Coder <wasymatieh01@gmail.com>
2024-06-01 23:17:57 +0200

    fix: move static vars outside the namespace

--- src/lw_instance.cc
+++ src/lw_instance.cc
@@ -22,19 +22,19 @@
 #include <iostream>
 #include <lw_instance.hh>
 
-namespace lw {
-  static constexpr auto app_name { "Test" };
-  static constexpr auto app_version { VK_MAKE_VERSION(0, 1, 0) };
-  static constexpr auto engine_name { "Longwang" };
-  static constexpr auto engine_version { VK_MAKE_VERSION(0, 1, 0) };
-  static constexpr auto api_version { VK_API_VERSION_1_3 };
-  static constexpr const char *extensions[] {{ VK_KHR_SURFACE_EXTENSION_NAME }};
-  static constexpr auto extension_count { sizeof(extensions) / sizeof(extensions[0]) };
+static constexpr auto app_name { "Test" };
+static constexpr auto app_version { VK_MAKE_VERSION(0, 1, 0) };
+static constexpr auto engine_name { "Longwang" };
+static constexpr auto engine_version { VK_MAKE_VERSION(0, 1, 0) };
+static constexpr auto api_version { VK_API_VERSION_1_3 };
+static constexpr const char *extensions[] {{ VK_KHR_SURFACE_EXTENSION_NAME }};
+static constexpr auto extension_count { sizeof(extensions) / sizeof(extensions[0]) };
 #ifndef NDEBUG
-  static constexpr const char *debug_layers[] {{ "VK_LAYER_KHRONOS_validation" }};
-  static constexpr auto debug_layer_count { sizeof(debug_layers) / sizeof(debug_layers[0]) };
+static constexpr const char *debug_layers[] {{ "VK_LAYER_KHRONOS_validation" }};
+static constexpr auto debug_layer_count { sizeof(debug_layers) / sizeof(debug_layers[0]) };
 #endif
 
+namespace lw {
   Instance::Instance(void) {
     vk::ApplicationInfo app_i { app_name, app_version, engine_name, engine_version, api_version };
     vk::InstanceCreateInfo instance_ci { {}, &app_i };
